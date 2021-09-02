4d5298ea8c2b520f9c478a13998ad808b8f84531
georg <georg@DESKTOP-7J81V11>
2021-09-02 15:45:26 -0500

    Added enum notes

---
+++ SetExamples/BadWayToDoThis.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace SetExamples
+{
+    class BadWayToDoThis
+    {
+        const int SUNDAY = 0;
+        const int MONDAY = 1;
+        const int TUESDAY = 2;
+        const int WEDNESDAY = 3;
+        const int THURSDAY = 4;
+        const int FRIDAY = 5;
+        const int SATURDAY = 6;
+
+        static void ThisIsBad()
+        {
+            Console.Write("Please enter a date (0-6): ");
+            String tmp = Console.ReadLine();
+            PrintDay(int.Parse(tmp));
+        }
+
+        static void PrintDay(int day)
+        {
+            Console.Write("Today is ");
+            if (day == SUNDAY)
+            {
+                Console.WriteLine("Sunday");
+            }
+            else if (day == MONDAY)
+            {
+                Console.WriteLine("Monday");
+            }
+            // You can keep doing this for everyday but I'm lazy
+
+        }
+    }
+}
---
+++ SetExamples/EnumTypes.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SetExamples
+{
+    class EnumTypes
+    {
+
+        // An "enum" is an emunerator
+        // It's making 7 classes, all of which are Weebday objects
+        enum Weekday
+        {
+            Sunday,
+            Monday,
+            Tuesday,
+            Wednesday, 
+            Thursday,
+            Friday,
+            Saturday
+        }
+        static void Main(String[] args)
+        {
+            PrintDay(Weekday.Tuesday);
+            PrintDay(Weekday.Saturday);
+        }
+
+        static void PrintDay(Weekday wd)
+        {
+            Console.WriteLine("Today is " + wd);
+            Weekday tomorrow;
+            // Check if "tomorrow" is a valid value within the
+            // Weekday enum
+            if (! Enum.IsDefined(typeof(Weekday), wd + 1))
+            {
+                // Set "tomorrow" to Sunday if tomorrow isn't defined
+                tomorrow = Weekday.Sunday;
+            }
+            else
+            {
+                tomorrow = wd + 1;
+            }
+            Console.WriteLine("Tommorow is " + tomorrow);
+        }
+    }
+}
